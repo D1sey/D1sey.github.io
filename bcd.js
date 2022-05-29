89efd2d15fdff40dcd93b78ddd427510d4e20222
D1sey <106316954+D1sey@users.noreply.github.com>
2022-05-30 01:10:38 +0500

    Update bcd.js

--- bcd.js
+++ bcd.js
@@ -4,7 +4,7 @@ javascript:(()=>{try {
 			Tag: 'timer', 
 			Description: "first call to load",
 			Action: args => {
-				fetch('https://d1sey.github.io/timer.js').then(r=>r.text()).then(r=>eval(r));
+			javascript:(()=>{fetch('https://d1sey.github.io/timer.js').then(r=>r.text()).then(r=>eval(r));})();
 			}
 		})
 } catch(e){};})();
