f027b16d4e9002da77c4815baf2f129c6740fbe0
D1sey <106316954+D1sey@users.noreply.github.com>
2022-06-07 02:02:32 +0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,49 +2,39 @@ javascript:(()=>{try {
 CommandCombine(
 		{
 			Tag: 'timer', 
-			Description: "[number] [not necessary number] [not necessary *emote] - starts direct or random timer with sound signal of finish (only you hear it)",
+			Description: "[one or a few numbers separated with space] [manual description instead default] - starts direct or random timer with sound signal of finish (only you hear it)",
 			Action: args => {
-let t = [30];
+let t = [];
 let j = NaN;
 let m = args;
 let l = m.length;
 if (m)
 	{
-  	if (m[0] == "*" || m[0] == ":") {m = m.slice(1);l = m.length};
-    if (m.slice(0,4) == "/me ") {m = m.slice(4);l = m.length};
-	for (let i = 1; i < 10; i++) 
-    {
-      if (m.slice(-i)*0+1)
-      {
-        j = m.slice(-i)*1;
-        l = m.length-i;
-      }
-    };
-	if (j) {t.pop();t.push(j)}
-    j = NaN;
-    m = m.slice(0,l);
-    for (let i = 1; i < 10; i++)
-    {
-      if (m.slice(-i)*0+1)
-      {
-        j = m.slice(-i)*1;
-        l = m.length-i;
-      }
-    }; 
-    if (j){if (j>t[0]) t.push(j);else t.unshift(j)};
-    m = m.slice(0,l);
-  };
+  m = m.split(" ");
+	while (m[0]*0+1) {
+  t.push(m.shift()*1)};
+  m = m.join(" ");
+  }
+        console.log(t);
+if (t.length==1 && t[0]=="") t[0] = 30;
+if (t.length==2) t.sort(function(a,b){return a-b});
+        console.log(t);
 if (!m && t.length==1) m = "starts "+t[0]+" sec timer";
 else if (!m && t.length==2) m = "starts random timer between "+t[0]+" and "+t[1]+" sec";
+else if (!m && t.length>2) m = "starts one of the following timers at random: "+t.join(" ,")+" sec";
 ServerSend("ChatRoomChat",{Content:m,Type:"Emote"});
 if (t.length==1) {
   j=t[0];
-  m="*"+Player.Name+"'s "+j+" sec timer is over";
+  m="*"+Player.Nickname+"'s "+j+" sec timer is over";
 }
 else if (t.length==2) {
   j=Math.floor((Math.random() * (t[1]-t[0]+1)) + t[0]);
-  m="*"+Player.Name+"'s random timer is over with "+j+" sec";
+  m="*"+Player.Nickname+"'s random timer is over with "+j+" sec";
 }
+else if (t.length>2) {
+  j=t[Math.floor((Math.random() * t.length))];
+  m="*"+Player.Nickname+"'s timer is over. It was "+j+" sec timer";}
+
 setTimeout(function timer()
     {
     ServerSend("ChatRoomChat",{Content:m,Type:"Emote"});
@@ -52,4 +42,5 @@ setTimeout(function timer()
      }, j*1000);
        }
 		})
+ServerSend("ChatRoomChat",{Content:"*Timer was loaded. Now command /timer works",Type:"Emote",Target:Player.MemberNumber});
 } catch(e){};})();
