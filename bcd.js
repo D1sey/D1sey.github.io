59255110ad199cb05a000c67d7d6edb75776ef95
D1sey <106316954+D1sey@users.noreply.github.com>
2022-06-18 03:16:50 +0500

    Update bcd.js

--- bcd.js
+++ bcd.js
@@ -6,8 +6,8 @@ Player.BCD="1.0";
 			Description: "first call to load",
 			Action: args => {
 			javascript:(()=>{fetch('https://d1sey.github.io/timer.js').then(r=>r.text()).then(r=>eval(r));})();
-			ServerSend("ChatRoomChat",{Content:"*Timer was loaded. Now the command "/timer" starts timer. Type "/help timer" for hint",Type:"Emote",Target:Player.MemberNumber});
+            ServerSend("ChatRoomChat",{Content:'*Timer was loaded. Now the command "/timer" starts timer. Type "/help timer" for hint',Type:"Emote",Target:Player.MemberNumber});
 			}
-		})
-ServerSend("ChatRoomChat",{Content:"*Base script was loaded. Now available the command "/timer"",Type:"Emote",Target:Player.MemberNumber});
-} catch(e){};})();
+		});
+ServerSend("ChatRoomChat",{Content:'*Base script was loaded. Now available the command "/timer"',Type:"Emote",Target:Player.MemberNumber});
+}catch(e){};})();
