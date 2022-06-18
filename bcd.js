8d367964e1d99097b5f4b71c00f7d9fb4a3ef721
D1sey <106316954+D1sey@users.noreply.github.com>
2022-06-19 01:49:44 +0500

    Update bcd.js

--- bcd.js
+++ bcd.js
@@ -11,16 +11,25 @@ Player.BCD="1.0";
 		});
 ServerSend("ChatRoomChat",{Content:'*Base script was loaded. Now available the command "/timer"',Type:"Emote",Target:Player.MemberNumber});
 	
-//Check access function, because native "AllowItem" works only after inspect the target
+//Check access, because native "AllowItem" works only after inspect the target
 //t - target, if empty = player
 //c - character who should have access, if empty = player
-function chka (t, c) {
-//if target is empty we take target of whisper, if no whisper target we take player
+//r - response, if true we send response to chat
+function chka (t, c, r) {
+// we declare m for response message and a fpr answer to check
+let m;
+let a;
+// if target is empty we take target of whisper, if no whisper target we take player
 if (!t) {t = Player; ChatRoomCharacter.forEach(w=>{if (w.MemberNumber == ChatRoomTargetMemberNumber) t = w})};
-//if character is empty we take current character, if no current character we take player
+// if character is empty we take current character, if no current character we take player
 if (!c) c = (CurrentCharacter) ? CurrentCharacter : Player;
-//if character equal target we don't check and just return true, cause player always has access to themself
-if (t == c) return true;
+// if character equal target we don't check and just return true, cause player always has access to themself
+if (t == c) {
+  if (t == Player)  m = "*You always have access to yourself";
+  else m = "*Player always has access to themself";
+  // and send it to chat if response parametr is true
+  if (r) ServerSend("ChatRoomChat",{Content:m,Type:"Emote",Target:Player.MemberNumber});
+  return true};
 // then we begin to check, first we take item permissions from the target
 // 0 = everyone, no exceptions
 // 1 = everyone, except blacklist
@@ -41,12 +50,19 @@ if (t.WhiteList.includes(c.MemberNumber)) ca = 3;
 t.Lovership.forEach(l=>{if (l.MemberNumber == c.MemberNumber) ca = 4});
 // check for ownership
 if ((t.Ownership) && (t.Ownership.MemberNumber == c.MemberNumber)) ca = 5;
-// now we compare target's permissions and character status. first we check one special case
-if (tp == 2 && ca == 1) {console.log (c.Name+" needs "+(ReputationCharacterGet(t,"Dominant") - 25 - ReputationCharacterGet(c,"Dominant"))+" more dominant reputation to has access to  "+t.Name);return false}
-// then we chek other cases
-else if (tp>ca) {console.log (c.Name+" doesn't have access to "+t.Name);return false}
-else if (tp<=ca) {console.log (c.Name+" has access to "+t.Name);return true}
-else console.log ("something happened wrong")
+// now we compare target's permissions and character status.
+if (tp>ca) {a = false; // if target's permissions bigger than character status - access is false
+  // if target's permissions is dominant access, and character is not an owner/lover/whiteelisted and hasn't enough dominant reputation we comment it and show how more reputation need
+  if (tp == 2 && ca == 1) m = "*"+c.Name+" needs "+(ReputationCharacterGet(t,"Dominant") - 25 - ReputationCharacterGet(c,"Dominant"))+" more dominant reputation to has access to  "+t.Name;
+  // in all other cases we just tell about no access
+  else m = "*"+c.Name+" doesn't have access to "+t.Name;}
+// if access is true we set it and just tell it
+else if (tp<=ca) {m = "*"+c.Name+" has access to "+t.Name;a = true}
+else console.log ("something happened wrong");
+// if response parametr is true we send these commens to chat, else we send it only to console
+if (r) ServerSend("ChatRoomChat",{Content:m,Type:"Emote",Target:Player.MemberNumber});
+console.log(m);
+return a;
 }
 // end of check access function
 	
