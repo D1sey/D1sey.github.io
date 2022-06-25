8cf7f3ef5b0a59bfea62c4043e9bc20539078d10
D1sey <106316954+D1sey@users.noreply.github.com>
2022-06-25 19:55:59 +0500

    Update wardrobe.js
    added arguments after 0 to strip specific clothes

--- wardrobe.js
+++ wardrobe.js
@@ -2,7 +2,7 @@ javascript:(()=>{try {
 ServerSend("ChatRoomChat",{Content:'*Wardrobe was loaded. Now the command \"/wardrobe\" works. Type \"/help wardrobe\" for hint',Type:"Emote",Target:Player.MemberNumber});
 CommandCombine({
 			Tag: 'wardrobe',
-    			Description: "How to use Disey\'s quick wardrobe: Write in the textbox \"/wardrobe\" with one of the follow options: \nA) You can use the slot number. \nB) You can use the complete name, part of the name or just a letter. \nC) You can search with numbers using \"-\" before it. \n# You can use \"0\" to strip one layer of clothes, \"00\" to strip two layers, etc.\n• Just using it without any text load the first slot. \n• If there are two or more coincidences, it display a list of all found outfits. \n• If there are more than 40 coincidences it ask for another keyword. \n• It works even if you are tied. \n• If you have selected target for whisper - this character will be dressed with chosen outfit (if you have access for this)",
+    			Description: "How to use Disey\'s quick wardrobe: Write in the textbox \"/wardrobe\" with one of the follow options: \nA) You can use the slot number. \nB) You can use the complete name, part of the name or just a letter. \nC) You can search with numbers using \"-\" before it. \n# You can use \"0\" to strip one layer of clothes, \"00\" to strip two layers, etc.\n# You can use name of cloth\/slot (or part of name or just a letter) after \"0\" to take off the specific clothes\n• Just using it without any text load the first slot. \n• If there are two or more coincidences, it display a list of all found outfits. \n• If there are more than 40 coincidences it ask for another keyword. \n• It works even if you are tied. \n• If you have selected target for whisper - this character will be dressed with chosen outfit or undressed (if you have access for this)",
 			Action: (_, command) => {
 			let [, ...message] = command.split(" "),
       m = message?.join(" "),
@@ -37,8 +37,23 @@ CommandCombine({
     };
   if (m)
     {
-      if (m[0]=="0" && m*1==0) {for (let i=0;i<m.length;i++) {CharacterAppearanceStripLayer(p)};ChatRoomCharacterUpdate(p);CharacterRefresh(p);return};
-      if (0 < m*1 && 1*m <= l) {o = 1*m-1;c = 1;w.push(" "+m+" - "+(Player.WardrobeCharacterNames[m*1-1]))}
+      if (m[0]=="0") {
+        if (m*1==0) {for (let i=0;i<m.length;i++) {CharacterAppearanceStripLayer(p)};ChatRoomCharacterUpdate(p);CharacterRefresh(p);return}
+        m = m.split(" ");
+        p.Appearance.forEach(a=>{
+          m.forEach(r=>{
+            if (a.Asset.Description.toLowerCase().match(r.toLowerCase()) || a.Asset.ArousalZone.toLowerCase().match(r.toLowerCase())) {
+              if (!["ItemNeck", "ItemNeckAccessories"].includes(a.Asset.Group.Name) && a.Asset.Group.Category === "Appearance" && a.Asset.Group.AllowNone) {
+                InventoryRemove(p, a.Asset.ArousalZone); w.push(a.Asset.Description);
+              } 
+            } 
+          })
+        })
+        if (w.length == 0) w = "*You don't have clothes with this keyword"; else w = w.join(", ");
+        ServerSend("ChatRoomChat",{Content:"*You took off next clothes: "+w,Type:"Emote",Target:pl});
+        return
+      }
+      else if (0 < m*1 && 1*m <= l) {o = 1*m-1;c = 1;w.push(" "+m+" - "+(Player.WardrobeCharacterNames[m*1-1]))}
       else if (1*m > l) {ServerSend("ChatRoomChat",{Content:"*You don't have so many wardrobe slots",Type:"Emote",Target:pl});return}
       else 
         {
